9002a21065ec43ff5c63680fb053c609b22c6a29
Daisuke Sakurai <d.sakurai0@gmail.com>
2025-11-29 05:47:21 +0000

    Add a C program

--- flake-hello/flake.nix
+++ flake-hello/flake.nix
@@ -5,8 +5,32 @@
 
   outputs = { self, nixpkgs }: {
     packages = {
-      x86_64-linux = {
-        hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      x86_64-linux = let
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+        };
+      in {
+        # Existing hello package
+        hello = pkgs.legacyPackages.x86_64-linux.hello;
+
+        # Custom hello.c package
+        HelloFlake = pkgs.stdenv.mkDerivation {
+          pname = "HelloFlakePName";
+          version = "1.0";
+
+          src = ./src;
+
+          buildInputs = [ pkgs.gcc ];
+
+          buildPhase = ''
+            gcc -o hello-flake hello.c
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp hello-flake $out/bin/
+          '';
+        };
       };
     };
   };
---
+++ flake-hello/src/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, Nix Flakes!\n");
+    return 0;
+}
