ac0dcbc687005c9843fcb2b886663f99b22d49e6
Daisuke Sakurai <d.sakurai0@gmail.com>
2025-12-21 05:04:34 +0000

    Rename folder

--- paraview/flake.nix
+++ paraview/flake.nix
@@ -1,12 +1,12 @@
 {
-  description = "A simple flake to test Nix installation";
+  description = "ParaView manual build environment";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/25.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/c8996fc1e6ef04c4049136c0363a8a4243f37c32";
 
-  outputs = { self, nixpkgs }: let
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-    };
+  outputs = { nixpkgs, ... }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    paraview = pkgs.paraview;
     
     # Minimal ospray derivation (replace version/hash as needed)
     ospray = pkgs.stdenv.mkDerivation {
@@ -30,43 +30,122 @@
         done
       '';
     };
-  in {
-    packages.x86_64-linux.default = ospray;
 
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      nativeBuildInputs = [
-        pkgs.curl
-        pkgs.pkg-config
-        pkgs.cmake
-        pkgs.perl
-      ];
-      buildInputs = [
-        pkgs.curl
-        pkgs.cmake
-        pkgs.linuxHeaders
-        pkgs.perl
-        #pkgs.ninja
-        # pkgs.pkg-config
-        #pkgs.tbb
-        #pkgs.embree
-        #pkgs.qt6.qtbase
-        #pkgs.qt6.qttools
-        #pkgs.mesa
-        #pkgs.libglvnd
-        #pkgs.qt6Packages.wrapQtAppsHook
-        pkgs.vcpkg
-        ospray
-      ];
-      shellHook = ''
-        # export PKG_CONFIG_EXECUTABLE="${pkgs.pkg-config}/bin/pkg-config"
-        export "ospray_DIR=${ospray}/lib/cmake/ospray"
-        export CMAKE_PREFIX_PATH="${ospray}:$CMAKE_PREFIX_PATH"
-        export CMAKE_LIBRARY_PATH="${ospray}/lib:$CMAKE_LIBRARY_PATH"
-        export CMAKE_INCLUDE_PATH="${ospray}/include:$CMAKE_INCLUDE_PATH"
-        export CURL="${pkgs.curl}/bin/curl"
-        export VCPKG_CURL="${pkgs.curl}/bin/curl"
-        echo "vcpkg and ospray are available. To use vcpkg, run: vcpkg install"
+  in {
+    devShells.${system}.default = paraview.overrideAttrs (oldAttrs: {
+      
+      # Modify postPatch - run original first, then add your changes
+      postPatch = ''
+        # When building paraview with external vtk, we can not infer resource_dir
+        # from the path of vtk's libraries. Thus hardcoding the resource_dir.
+        # See https://gitlab.kitware.com/paraview/paraview/-/issues/23043.
+        # substituteInPlace Remoting/Core/vtkPVFileInformation.cxx \
+        #   --replace-fail "return resource_dir;" "return \"$out/share/paraview\";"
+
+        # fix build against qt-6.10.1
+        substituteInPlace Qt/Core/{pqFlatTreeViewEventTranslator,pqQVTKWidgetEventTranslator}.cxx \
+          ThirdParty/QtTesting/vtkqttesting/{pqAbstractItemViewEventTranslator,pqBasicWidgetEventTranslator}.cxx \
+          --replace-fail "mouseEvent->buttons()" "static_cast<int>(mouseEvent->buttons())" \
+          --replace-fail "mouseEvent->modifiers()" "static_cast<int>(mouseEvent->modifiers())"
+        echo "Running custom postPatch modifications..."
       '';
-    };
+
+      nativeBuildInputs = [
+        pkgs.cmake
+        pkgs.ninja
+        pkgs.qt6Packages.wrapQtAppsHook
+        pkgs.makeWrapper
+        pkgs.xorg.xeyes # Useful for debugging GUI
+      ];
+      
+      propagatedBuildInputs = [
+        pkgs.qt6Packages.qttools
+        pkgs.qt6Packages.qt5compat
+        pkgs.protobuf
+        pkgs.nlohmann_json
+        # pkgs.vtk-full
+        pkgs.mesa
+        ospray
+        pkgs.libglvnd
+        pkgs.xorg.libxcb
+        pkgs.xorg.xcbutilcursor
+        pkgs.xorg.xcbutilimage
+        pkgs.xorg.xcbutilkeysyms
+        pkgs.xorg.xcbutilrenderutil
+        pkgs.xorg.xcbutilwm
+      ];
+      
+      cmakeFlags = [
+        (pkgs.lib.cmakeBool "PARAVIEW_ENABLE_RAYTRACING" true)
+
+        (pkgs.lib.cmakeBool "PARAVIEW_VERSIONED_INSTALL" false)
+        (pkgs.lib.cmakeBool "PARAVIEW_BUILD_WITH_EXTERNAL" false)
+        (pkgs.lib.cmakeBool "PARAVIEW_USE_EXTERNAL_VTK" false)
+        (pkgs.lib.cmakeBool "PARAVIEW_USE_QT" true)
+        (pkgs.lib.cmakeBool "PARAVIEW_USE_MPI" false)
+        (pkgs.lib.cmakeBool "PARAVIEW_USE_PYTHON" false)
+        (pkgs.lib.cmakeBool "PARAVIEW_ENABLE_WEB" false)
+        (pkgs.lib.cmakeBool "PARAVIEW_ENABLE_CATALYST" false)
+        (pkgs.lib.cmakeBool "PARAVIEW_ENABLE_VISITBRIDGE" false)
+        (pkgs.lib.cmakeBool "PARAVIEW_ENABLE_ADIOS2" false)
+        (pkgs.lib.cmakeBool "PARAVIEW_ENABLE_FFMPEG" false)
+        (pkgs.lib.cmakeBool "PARAVIEW_ENABLE_FIDES" false)
+        (pkgs.lib.cmakeBool "PARAVIEW_ENABLE_ALEMBIC" false)
+        (pkgs.lib.cmakeBool "PARAVIEW_ENABLE_LAS" false)
+        (pkgs.lib.cmakeBool "PARAVIEW_ENABLE_GDAL" false)
+        (pkgs.lib.cmakeBool "PARAVIEW_ENABLE_PDAL" false)
+        (pkgs.lib.cmakeBool "PARAVIEW_ENABLE_OPENTURNS" false)
+        (pkgs.lib.cmakeBool "PARAVIEW_ENABLE_MOTIONFX" false)
+        (pkgs.lib.cmakeBool "PARAVIEW_ENABLE_OCCT" false)
+        (pkgs.lib.cmakeBool "PARAVIEW_ENABLE_XDMF3" false)
+        (pkgs.lib.cmakeFeature "CMAKE_INSTALL_BINDIR" "bin")
+        (pkgs.lib.cmakeFeature "CMAKE_INSTALL_LIBDIR" "lib")
+        (pkgs.lib.cmakeFeature "CMAKE_INSTALL_INCLUDEDIR" "include")
+        (pkgs.lib.cmakeFeature "CMAKE_INSTALL_DOCDIR" "share/paraview/doc")
+        "-GNinja"
+        "-Dospray_DIR=${ospray}/lib/cmake/ospray"
+      ];
+      
+      # Don't build nor install automatically.
+      # Not sure if these really have an effect.
+      dontBuild   = true;
+      dontInstall = false;
+
+      shellHook = ''
+        # Set up Qt and OpenGL environment
+        export QT_PLUGIN_PATH="${pkgs.qt6Packages.qtbase}/${pkgs.qt6Packages.qtbase.qtPluginPrefix}"
+        export QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt6Packages.qtbase}/${pkgs.qt6Packages.qtbase.qtPluginPrefix}/platforms"
+        export QT_QPA_PLATFORM=offscreen
+        export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ 
+          pkgs.mesa 
+          pkgs.libglvnd 
+          pkgs.xorg.xcbutilcursor
+          pkgs.xorg.libxcb
+          pkgs.qt6Packages.qtbase
+        ]}:$LD_LIBRARY_PATH"
+        export __GLX_VENDOR_LIBRARY_NAME=mesa
+        export GALLIUM_DRIVER=llvmpipe
+        export LIBGL_ALWAYS_SOFTWARE=1
+        export MESA_GL_VERSION_OVERRIDE=3.3
+        export FONTCONFIG_FILE="${pkgs.fontconfig.out}/etc/fonts/fonts.conf"
+        export LC_ALL=C.UTF-8
+        
+        echo "ParaView development shell"
+        echo "Source is available at: $src"
+        echo ""
+        echo "To build manually:"
+        echo "  unpackPhase"
+        echo "  cd ParaView-v6.0.1"
+        echo "  patchPhase"
+        echo "  mkdir -p build && cd build"
+        echo "  cmake \$cmakeFlags .."
+        echo "  ninja"
+        echo ""
+        echo ""
+        echo "CMake flags: $cmakeFlags"
+        
+        echo "Run ParaView-v6.0.1/build/bin/paraview with nix develop (without -i)"
+      '';
+    });
   };
 }
